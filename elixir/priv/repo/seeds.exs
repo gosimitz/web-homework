07c65a6c2c5c4c3fe5d1113ac2ae6ac2bd99541a
gosimitz <>
2021-01-10 14:33:36 -0700

    Added transaction and user seeds, now working on automating it so the transactions can be created randomly based on users/merchants in the database.

--- elixir/priv/repo/seeds.exs
+++ elixir/priv/repo/seeds.exs
@@ -9,6 +9,10 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Homework.Users
 alias Homework.Merchants
+alias Homework.Transactions
 
-Merchants.create_merchant(%{name: "Gob Bluth", description: "Character"})
+Merchants.create_merchant(%{name: "Gene", description: "Private Investigator"})
+Users.create_user(%{dob: "1-23-1234", first_name: "Maebe", last_name: "Funke"})
+Transactions.create_transaction(%{user_id: "1a110e2b-a79f-4a24-abec-11cfd092ca0d", merchant_id: "f0457fc7-811e-4fcc-a0b8-a8589127212a", amount: 250, debit: true, description: "One Bluth Frozen Banana"})
