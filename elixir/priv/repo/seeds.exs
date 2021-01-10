89117e3266304954385638b44c77bbea580cabe4
gosimitz <>
2021-01-10 16:23:16 -0700

    Implemented an automatic transaction generator based on User.id's. Next steps is randomly selecting merchants for each transaction.

--- elixir/priv/repo/seeds.exs
+++ elixir/priv/repo/seeds.exs
@@ -12,7 +12,13 @@
 alias Homework.Users
 alias Homework.Merchants
 alias Homework.Transactions
+alias Homework.Repo
 
 Merchants.create_merchant(%{name: "Gene", description: "Private Investigator"})
-Users.create_user(%{dob: "1-23-1234", first_name: "Maebe", last_name: "Funke"})
-Transactions.create_transaction(%{user_id: "1a110e2b-a79f-4a24-abec-11cfd092ca0d", merchant_id: "f0457fc7-811e-4fcc-a0b8-a8589127212a", amount: 250, debit: true, description: "One Bluth Frozen Banana"})
+Users.create_user(%{dob: "1-23-1234", first_name: "Plant", last_name: "Veal"})
+user_data = Users.list_users([])
+#Fetch the users from the database and create random Transactions with their ids.
+Enum.each(user_data, fn(data) ->
+  id = data.id
+  Transactions.create_transaction(%{user_id: id, merchant_id: "f0457fc7-811e-4fcc-a0b8-a8589127212a", amount: 801, debit: true, description: "One Bluth Frozen Banana"})
+end)
